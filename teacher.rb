bd2cff0d7ba2ecb0c35d3f39df8b657ad68532f6
Yerimah <danielyerimah@yahoo.com>
2022-04-11 16:21:22 +0100

    modify person.rb and README.md

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'unknown')
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
     @specialization = specialization
-    super(age, name)
+    super(age, name, parent_permission)
   end
 
   def can_use_services?
