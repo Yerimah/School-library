778c21d5a3b4fb9a2f64809d54c7db949e537b20
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-18 14:12:14 +0000

    Fix errors spotted by linters

--- console.rb
+++ console.rb
@@ -1,44 +1,44 @@
-class Console 
-    def console_entry_point
-        puts 'Welcome to my School Library'
-        until list_of_options
-          input = gets.chomp
-          if input == '7'
-            puts 'Thanks for using my School Library App'
-            break
-          end
-          option input
-        end
+class Console
+  def console_entry_point
+    puts 'Welcome to my School Library'
+    until list_of_options
+      input = gets.chomp
+      if input == '7'
+        puts 'Thanks for using my School Library App'
+        break
       end
-
-      def list_of_options
-        puts
-        puts 'Choose an option by entering a number: '
-        puts '1 List all books'
-        puts '2 List all people'
-        puts '3 Create a person'
-        puts '4 Create a book'
-        puts '5 Create a rental'
-        puts '6 List all rentals for a given person id'
-        puts '7 Exit'
-      end
-      
-      def option(input)
-        case input
-        when '1'
-          list_all_books
-        when '2'
-          list_all_persons
-        when '3'
-          create_person
-        when '4'
-          create_book
-        when '5'
-          create_rental
-        when '6'
-          list_all_rentals
-        else
-          puts 'Please enter a number between 1 and 7.'
-        end
-      end
+      option input
     end
+  end
+
+  def list_of_options
+    puts
+    puts 'Choose an option by entering a number: '
+    puts '1 List all books'
+    puts '2 List all people'
+    puts '3 Create a person'
+    puts '4 Create a book'
+    puts '5 Create a rental'
+    puts '6 List all rentals for a given person id'
+    puts '7 Exit'
+  end
+
+  def option(input)
+    case input
+    when '1'
+      list_all_books
+    when '2'
+      list_all_persons
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_all_rentals
+    else
+      puts 'Please enter a number between 1 and 7.'
+    end
+  end
+end
